0a59a02e2140cd20f82e3700614c9310e855775e
Thorsteinn19 <70671288+Thorsteinn19@users.noreply.github.com>
2021-08-08 00:57:24 +0200

    Starting Frame

--- R-project.py
+++ R-project.py
@@ -1,2 +1,40 @@
 import tkinter as tk
+
+border_effects = {
+    "flat": tk.FLAT,
+}
+
 window =tk.Tk()
+
+
+def handle_click(event):
+    print(entry.get())
+
+frame_a= tk.Frame()
+frame_b=tk.Frame()
+
+greeting = tk.Label(master=frame_a,
+    text="Prufutexti",
+    fg="white",
+    bg="#34A2FE",
+    width=43,
+    height=1
+)
+
+greeting.pack()
+
+entry = tk.Entry(master=frame_b,
+    fg="black", 
+    bg="white",
+    width=50
+)
+entry.pack()
+
+frame_a.pack()
+frame_b.pack()
+
+button = tk.Button(master=window,text="Click me!")
+button.pack()
+button.bind("<Button-1>", handle_click)
+window.mainloop()
+
