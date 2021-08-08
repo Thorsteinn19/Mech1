b73bef0ce7239edacfeb90b28f90ff9962ff9b83
Thorsteinn19 <70671288+Thorsteinn19@users.noreply.github.com>
2021-08-08 15:04:49 +0200

    Framing fixed

--- R-project.py
+++ R-project.py
@@ -1,40 +1,42 @@
 import tkinter as tk
-
-border_effects = {
-    "flat": tk.FLAT,
-}
-
-window =tk.Tk()
-
+from tkinter.constants import END
 
 def handle_click(event):
+    name=entry.get()
     print(entry.get())
+    entry.delete(0,END)
+
+window =tk.Tk()
+window.geometry("400x400")
 
 frame_a= tk.Frame()
 frame_b=tk.Frame()
 
 greeting = tk.Label(master=frame_a,
-    text="Prufutexti",
-    fg="white",
-    bg="#34A2FE",
+    text="Sláðu inn nafn",
+    fg="black",
+    bg="white",
     width=43,
-    height=1
+    height=5
 )
-
-greeting.pack()
 
 entry = tk.Entry(master=frame_b,
     fg="black", 
     bg="white",
-    width=50
+    width=50,
 )
+
+entry.bind("<Return>",handle_click)
+
+button = tk.Button(master=window,text="Staðfesta")
+
+button.bind("<Button-1>", handle_click)
+
+greeting.pack()
 entry.pack()
 
 frame_a.pack()
 frame_b.pack()
-
-button = tk.Button(master=window,text="Click me!")
 button.pack()
-button.bind("<Button-1>", handle_click)
 window.mainloop()
 
